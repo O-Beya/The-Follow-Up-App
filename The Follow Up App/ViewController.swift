9560a716fc40fb9d913599dcf571e795d8271a07
O-Beya <olivierbeya@gmail.com>
2017-04-01 06:18:49 -0500

    Initial Commit

---
+++ The Follow Up App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Follow Up App
+//
+//  Created by Madavia on 4/1/17.
+//  Copyright © 2017 OlivierBeya(Beyasoft). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ The Follow Up AppTests/The_Follow_Up_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Follow_Up_AppTests.swift
+//  The Follow Up AppTests
+//
+//  Created by Madavia on 4/1/17.
+//  Copyright © 2017 OlivierBeya(Beyasoft). All rights reserved.
+//
+
+import XCTest
+@testable import The_Follow_Up_App
+
+class The_Follow_Up_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
